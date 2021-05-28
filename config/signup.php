13e0703ada529efb5e9027defeaa8b55dcf0ab75
gordon <gordon@sintara.co.za>
2021-05-28 09:09:23 +0200

    -

--- config/signup.php
+++ config/signup.php
@@ -5,3 +5,15 @@
     $last_name = mysqli_real_escape_string($conn, $_POST['surname']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+
+    if(!empty($name) && !empty($last_name) && !empty($email) && !empty($password)){
+        // is email valid?
+        if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+            // is valid 
+        }else{
+            echo $email." is not a valid email address!";
+        }
+    }else{
+        echo "All fields are required";
+    }
